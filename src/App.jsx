abaaa80c92821a0cb265bbe3b2c6f63403d75b4b
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-24 23:56:28 +0530

    chore: read directory and play individual song from it.(this is just for testing ideas tho)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import { useState,useEffect,useRef } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
-  const [audioURL,setAudioURL] = useState(null);
-  const audioRef = useRef(null);
+  const [filePath,setFilePath] = useState(null);
 
   function handleInput(e){
-    const url = (e.target.files[0]);
-    if(url){
-      setAudioURL(URL.createObjectURL(url));
-    }
+    const files = e.target.files;
+    const filesArray = Array.from(files)
+    console.log(filesArray[0]);
+    setFilePath(filesArray);
   }
 
   return (
       <>
         <h2>Choose a song...</h2>
-        <input type='file' accept='audio/*' onChange={handleInput}>
+        <input type='file' accept='audio/*' webkitdirectory='true' multiple onChange={handleInput}>
         </input>
-          {audioURL && (
-            <>
-            <br></br>
-            <audio ref={audioRef} controls src={audioURL}></audio>
+        {
+        
+          filePath && filePath.map(file => {
+            return <>
+              <label>{file.name}</label>
+              <audio controls src={URL.createObjectURL(file)} key={uuidv4()}>{file.name}</audio>
+              <br></br>
             </>
-          )}
+
+          })
+
+        }
       </>
   )
 }
